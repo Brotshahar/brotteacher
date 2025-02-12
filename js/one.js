a57fe6db7a4a3ded14e8c9245199609523a4f531
Brotshahar <137767931+Brotshahar@users.noreply.github.com>
2025-02-12 21:27:23 +0200

    js

---
+++ js/one.js
@@ -0,0 +1,32 @@
+console.log ('Thank You');
+let x = 5;
+let y = 5;
+
+let result = x + y;
+if (x === y) {
+    console.log('BOOM');
+}
+
+console.log(result);
+
+let day = 3;
+
+if (day === 1){
+    console.log('Sunday');
+}
+else if (day === 2) {
+    console.log('Monday');
+}
+else  {
+    console.log('Thuseday');
+};
+
+if (day === 1 ) {
+    console.log('Have a nice week');
+}
+else if ((day > 1 ) & ( day <5 )) {
+    console.log('Good day');
+}
+else {
+    console.log('Weekend');
+};
