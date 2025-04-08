c5bd5eefc414f579ec072fd3f91081c8b2f35337
Brotshahar <137767931+Brotshahar@users.noreply.github.com>
2025-04-08 16:13:40 +0300

    Update source.js

--- www/letterh/source.js
+++ www/letterh/source.js
@@ -3,7 +3,7 @@ const color = document.querySelector("#square");
 
 
 function suffule(){
-    const randomOne = Math.floor(Math.random() * 26) ;
+    const randomOne = Math.floor(Math.random() * 25) + 1;
     color.src = "./asset/" + randomOne + ".png";
 
 }
